bee58ff4bab6f476eef0a76e6fbc7d50ed4181f0
Hubert Łępicki <hubert.lepicki@amberbit.com>
2022-12-07 12:22:48 +0100

    Version bump

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule StateChannel.MixProject do
     [
       app: :state_channel,
       description: @description,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
